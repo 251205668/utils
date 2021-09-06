71f04e1253da56081b81d91fdedc322e5b5efc04
杨志豪 <yangzhihao03@zuoyebang.com>
2021-09-06 19:23:24 +0800

    feat: delete tests

--- src/templates.ts
+++ src/templates.ts
@@ -2,11 +2,11 @@ export interface TemplateOptions {
   /***
    * Argument name to use in template function
    */
-  dataName: string
+  dataName: string;
   /***
    * Output array name for use in template function
    */
-  outName: string
+  outName: string;
 }
 
 /***
@@ -16,23 +16,30 @@ export interface TemplateOptions {
  * @param {{dataName:string,outName:string}}? options Options for template
  * @returns {(data: any) => string} Template function
  */
-export function template (template: string, { dataName = '$__', outName = 'out' }: Partial<TemplateOptions> = {}): (data: any) => string {
-  const re = /{{(.+?)}}/g
-  const reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g
-  let code = `with(${dataName}) {var ${outName}=[];\n`
-  let cursor = 0
-  let match
+export function template(
+  template: string,
+  { dataName = "$__", outName = "out" }: Partial<TemplateOptions> = {}
+): (data: any) => string {
+  const re = /{{(.+?)}}/g;
+  const reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
+  let code = `with(${dataName}) {var ${outName}=[];\n`;
+  let cursor = 0;
+  let match;
   const add = function (line: string, js?: boolean) {
-    js ? (code += line.match(reExp) ? line + '\n' : `try{${outName}.push(` + line + ')}catch(_unused){};\n') : (code += line !== '' ? `${outName}.push("` + line.replace(/"/g, '\\"') + '");\n' : '')
-    return add
-  }
+    js
+      ? (code += line.match(reExp)
+          ? line + "\n"
+          : `try{${outName}.push(` + line + ")}catch(_unused){};\n")
+      : (code += line !== "" ? `${outName}.push("` + line.replace(/"/g, '\\"') + '");\n' : "");
+    return add;
+  };
 
   // tslint:disable-next-line no-conditional-assignment
-  while (match = re.exec(template)) {
-    add(template.slice(cursor, match.index))(match[1], true)
-    cursor = match.index + match[0].length
+  while ((match = re.exec(template))) {
+    add(template.slice(cursor, match.index))(match[1], true);
+    cursor = match.index + match[0].length;
   }
-  add(template.substr(cursor, template.length - cursor))
-  code += `return ${outName}.join("");}`
-  return new Function(dataName, code.replace(/[\r\t\n]/g, '')) as (data: any) => string
+  add(template.substr(cursor, template.length - cursor));
+  code += `return ${outName}.join("");}`;
+  return new Function(dataName, code.replace(/[\r\t\n]/g, "")) as (data: any) => string;
 }
